19b2a6cb8eeb8f14737db3c97df0f4e081f1cccc
Bryan Gruneberg <hello@bryangruneberg.com>
2025-02-26 17:00:25 -0600

    Adding in code to check environment variable

--- .lagoon/scripts/scaffold.sh
+++ .lagoon/scripts/scaffold.sh
@@ -6,38 +6,45 @@ source /lagoon/entrypoints/50-dotenv.sh
 # Generate some additional enviornment variables
 source /lagoon/entrypoints/55-generate-env.sh
 
+if [ ! -z "$SERVICE_NAME" ]; then
+  RUN_CONTEXT=$RUN_CONTEXT.$SERVICE_NAME
+  if [ ! -z "$SERVICE_NAME" ]; then
+    RUN_CONTEXT=$RUN_CONTEXT.$SERVICE_NAME.$LAGOON_KUBERNETES 
+  fi
+fi
+
 FLAG_FILE="/app/scaffolding_done.flag"
 
 cd /app
 
 echo "----------------------------------------------"
-echo "-------- Variables in $SERVICE "
+echo "-------- Variables in $RUN_CONTEXT "
 echo "----------------------------------------------"
 export
 echo "----------------------------------------------"
 
 # Check if the flag file exists
 if [ ! -f "$FLAG_FILE" ]; then
-    echo "*** NO SCAFFOLD FILE FOUND: $SERVICE***"
+    echo "*** NO SCAFFOLD FILE FOUND: $RUN_CONTEXT ***"
     git config --global --add safe.directory /app
     if [ -z "$LAGOON_KUBERNETES" ]; then
-        echo "Not running in Lagoon - we will try install in $SERVICE"
+        echo "Not running in Lagoon - we will try install in $RUN_CONTEXT"
         composer install --no-dev
         cp -r /app/vendor/drupal/cms/web/profiles/drupal_cms_installer /app/web/profiles/
         cp /app/drush/Commands/contrib/drupal_integrations/assets/* /app/web/sites/default
     fi    
     # Create the flag file to indicate the script has run
-    echo "About to create $FLAG_FILE in $SERVICE"
+    echo "About to create $FLAG_FILE in $RUN_CONTEXT"
     touch "$FLAG_FILE"
 else
-    echo "*** SCAFFOLD FILE FOUND: $SERVICE ***"
-    echo "The initialization script has already run on $SERVICE."
+    echo "*** SCAFFOLD FILE FOUND: $RUN_CONTEXT ***"
+    echo "The initialization script has already run on $RUN_CONTEXT."
 fi
 
 # here we attempt to copy the details
 if [ ! -f "/app/web/sites/default/settings.php" ]; then
-    echo "Copying the details in $SERVICE"
+    echo "Copying the details in $RUN_CONTEXT"
     cp /app/.lagoon/assets/* /app/web/sites/default
     mv /app/web/sites/default/initial.settings.php /app/web/sites/default/settings.php
-    echo "Details copied in $SERVICE"
+    echo "Details copied in $RUN_CONTEXT"
 fi
